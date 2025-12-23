91a44e8ccc4242fdb70730a92b8f027b766177ba
daeya0406 <daeya0406@gmail.com>
2025-12-24 01:48:05 +0900

    feat: 빌드 오류 개선

--- src/types/react-syntax-highlighter.d.ts
+++ src/types/react-syntax-highlighter.d.ts
@@ -1,10 +1,13 @@
-declare module 'react-syntax-highlighter/dist/cjs/styles/prism' {
-  export const duotoneLight: unknown;
-  export const duotoneDark: unknown;
+declare module 'react-syntax-highlighter' {
+  export const Prism: any;
+  export const Light: any;
+  const SyntaxHighlighter: any;
+  export default SyntaxHighlighter;
 }
 
-declare module 'react-syntax-highlighter' {
-  export const Prism: unknown;
-  const DefaultExport: unknown;
-  export default DefaultExport;
+declare module 'react-syntax-highlighter/dist/cjs/styles/prism' {
+  export const duotoneLight: any;
+  export const duotoneDark: any;
+  const styles: any;
+  export default styles;
 }
